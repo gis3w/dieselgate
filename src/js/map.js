7697ac33a1618932ac3a41998bfaf6eb522b8da6
volterra79 <francescoboccacci@libero.it>
2018-03-01 18:44:33 +0100

    added first popup

--- src/js/map.js
+++ src/js/map.js
@@ -9,10 +9,51 @@ var OSMLayer = new ol.layer.Tile({
   })
 });
 
+var container = document.getElementById('popup');
+var content = document.getElementById('popup-content');
+var closer = document.getElementById('popup-closer');
+
+
+closer.onclick = function() {
+  overlay.setPosition(undefined);
+  closer.blur();
+  return false;
+};
+
+var overlay = new ol.Overlay({
+  element: container,
+  autoPan: true,
+  autoPanAnimation: {
+    duration: 250
+  }
+});
+
 var map = new ol.Map({
   target: "map",
   view: view,
+  overlays: [overlay],
   layers: [OSMLayer, reticolo_layer, stati_layer, city2_layer]
+});
+
+
+/**
+ * Add a click handler to the map to render the popup.
+ */
+map.on('singleclick', function(evt) {
+  var pixel = evt.pixel;
+  var coordinate = evt.coordinate;
+  this.forEachFeatureAtPixel(pixel, function(feature, layer) {
+    if (feature) {
+      content.innerHTML = '<p>'+feature.get('Country')+'</p>';
+      overlay.setPosition(coordinate);
+    }
+    return feature
+  }, {
+    layerFilter: function(layer) {
+     return layer == reticolo_layer
+    },
+    hitTolerance: 10
+  });
 });
 
 
